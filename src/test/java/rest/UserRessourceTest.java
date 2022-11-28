b06eb7f608725392403b1970355ec391744a04c9
acmve <acmverden@hotmail.com>
2022-11-28 19:19:54 +0100

    userRessourceTest

---
+++ src/test/java/rest/UserRessourceTest.java
@@ -0,0 +1,81 @@
+package rest;
+
+import entities.User;
+import io.restassured.RestAssured;
+import io.restassured.parsing.Parser;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import utils.EMF_Creator;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.core.UriBuilder;
+import java.net.URI;
+
+public class UserRessourceTest {
+
+    private static final int SERVER_PORT = 7777;
+    private static final String SERVER_URL = "http://localhost/api";
+
+    static final URI BASE_URI = UriBuilder.fromUri(SERVER_URL).port(SERVER_PORT).build();
+    private static HttpServer httpServer;
+    private static EntityManagerFactory emf;
+
+    static HttpServer startServer() {
+        ResourceConfig rc = ResourceConfig.forApplication(new ApplicationConfig());
+        return GrizzlyHttpServerFactory.createHttpServer(BASE_URI, rc);
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+        //This method must be called before you request the EntityManagerFactory
+        EMF_Creator.startREST_TestWithDB();
+        emf = EMF_Creator.createEntityManagerFactoryForTest();
+
+        httpServer = startServer();
+        //Setup RestAssured
+        RestAssured.baseURI = SERVER_URL;
+        RestAssured.port = SERVER_PORT;
+        RestAssured.defaultParser = Parser.JSON;
+    }
+
+    @AfterAll
+    public static void closeTestServer() {
+        //Don't forget this, if you called its counterpart in @BeforeAll
+        EMF_Creator.endREST_TestWithDB();
+
+        httpServer.shutdownNow();
+    }
+
+    // Setup the DataBase (used by the test-server and this test) in a known state BEFORE EACH TEST
+    //TODO -- Make sure to change the EntityClass used below to use YOUR OWN (renamed) Entity class
+    @BeforeEach
+    public void setUp() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            //Delete existing users and roles to get a "fresh" database
+            em.createQuery("delete from User").executeUpdate();
+
+            User user = new User("user", "test","Mogens", 20202020, "Værebrovej 18", 2880);
+
+            User admin = new User("admin", "test","Konrad", 30303030, "Liljevej 13", 2900);
+
+            admin.setRole("admin");
+            em.persist(user);
+            em.persist(admin);
+
+            //System.out.println("Saved test data to database");
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+
+
+
+}
